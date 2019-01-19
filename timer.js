fabef168e5fb2b6c2168db5ca7d6a1d2f3d237bd
snowbar <snowbarlpt+tools@gmail.com>
2019-01-20 03:30:41 +0900

    add:timer

---
+++ timer.js
@@ -0,0 +1,54 @@
+(function(){
+  'use strict';
+  const total = document.getElementById('total-time');
+  const task = [document.getElementById('task-time'),document.getElementById('creating-time'),document.getElementById('gathering-time'),document.getElementById('sport-time'),document.getElementById('reading-time'),document.getElementById('break-time')];
+  const buttons = document.querySelectorAll('button');
+  const totaltime = timecount(total);
+  let hour = 0;
+  let minute = 0;
+  let second = 1;
+  for(let i = 0;i<buttons.length;i++){
+    buttons[i].onclick = () => {
+      const rap = document.createElement('span');
+      rap.innerText = addZero(hour) +':'+ addZero(minute) +':'+  addZero(second)+ ' ';
+      task[i].appendChild(rap);
+      hour = 0;
+      minute = 0;
+      second = 0;
+    }
+  }
+
+  function timecount(timer) {
+
+    timer.innerText = "00:00:00";
+    //sportButton.onclick = rapTime();
+    let countInterval = setInterval(countup,1000);
+    function countup() {
+      if(minute == 60){
+        hour++;
+        minute = 0;
+      }
+      if(second == 60){
+        minute++;
+        second = 0
+      }
+      timer.innerText = addZero(hour) +':'+ addZero(minute) +':'+ addZero(second);
+      second++;
+    }
+    /*function rapTime(){
+      const rap = document.createElement('span');
+      rap.innerText = addZero(hour) +':'+ addZero(minute) +':'+ addZero(second)+ ' ';
+      sport.appendChild(rap);
+      hour = 0;
+      minute = 0;
+      second = 0;
+    }*/
+  }
+  function addZero(time) {
+    if(time < 10){
+      return '0' + time;
+    } else {
+      return time;
+    }
+  }
+}());
